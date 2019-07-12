1e4deea7457dad24dec1fefe17e4965ddc9a5918
Limengbo <15600099705@163.com>
2019-07-12 10:59:57 +0800

    增加全景图观看

--- src/js/panorama.js
+++ src/js/panorama.js
@@ -1,36 +1,27 @@
 import '../style/panorama';
 import '../style/style';
-// import PhotoSphereViewer from '../utils/photo-sphere-viewer.min';
-// console.log(new PhotoSphereViewer())
-// import img from '../static/quanjing/beimen.jpg';
-// var PhotoSphereViewer = require('../utils/photo-sphere-viewer.min.js');
-// window.onload = function () {
-//     getTitleHeight();
-//     loadingAllImg();
-// }
-// //让全景图刚好撑满屏幕
-// var canvasHeight = 0;
-// function getTitleHeight() {
-//     var maxHeight = window.innerHeight;
-//     canvasHeight = maxHeight + 'px';
-// }
+import img from '../static/quanjing/beimen.jpg'
+var div = document.getElementById('panorama');
 
-// //全景图参数配置函数
-// function loadingAllImg() {
-//     var div = document.getElementById('container');
-//     var PSV = new PhotoSphereViewer({
-//         // 全景图的完整路径
-//         panorama: img,
-//         // 放全景图的元素
-//         container: div,
-//         // 可选，默认值为2000，全景图在time_anim毫秒后会自动进行动画。（设置为false禁用它）
-//         time_anim: 0,
-//         // 可选值，默认为false。显示导航条。
-//         navbar: true,
-//         // 可选，默认值null，全景图容器的最终尺寸。例如：{width: 500, height: 300}。
-//         size: {
-//             width: '100%',
-//             height: canvasHeight
-//         }
-//     });
-// }
+var PSV = new PhotoSphereViewer({
+  // Panorama, given in base 64
+  panorama: img,
+
+  // Container
+  container: div,
+
+  // Deactivate the animation
+  time_anim: false,
+
+  // Display the navigation bar
+  navbar: false,
+
+  // Resize the panorama
+  size: {
+    width: '100%',
+    height: '100%'
+  },
+
+  // No XMP data
+  usexmpdata: false
+});
